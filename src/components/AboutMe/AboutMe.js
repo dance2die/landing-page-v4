c8fe455d8199fa5255309f75e4471e15c8e922f5
dance2die <sungmkim80@gmail.com>
2019-04-25 15:34:36 -0400

    Added font awesome fonts

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,20 +1,36 @@
 import React from 'react'
+import {
+  FaTwitter,
+  FaStackOverflow,
+  FaInstagram,
+  FaEnvelope,
+} from 'react-icons/fa'
 
 import Avatar from '../Avatar'
 
 import './AboutMe.scss'
 
 const Name = () => <h2 className="name">Sung M. Kim</h2>
-const Divider = () => <div className="divider">Divider</div>
-const Detail = () => <div className="detail">Detail</div>
-const Social = () => <div className="social">Social</div>
+const Detail = () => (
+  <div className="detail">
+    <div>An Infinite Learner</div>
+    <div>Currently focusing on Front-end development</div>
+  </div>
+)
+const Social = () => (
+  <div className="social">
+    <FaTwitter />
+    <FaInstagram />
+    <FaEnvelope />
+    <FaStackOverflow />
+  </div>
+)
 
 function AboutMe() {
   return (
     <div className="about-me">
       <Avatar />
       <Name />
-      <Divider />
       <Detail />
       <Social />
     </div>
